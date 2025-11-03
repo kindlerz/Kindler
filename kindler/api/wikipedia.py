cf0d991293bc7f354bb6f2f3cd8ae5eb8eb94eab
kasramp <kasra_mp@yahoo.com>
2025-11-03 23:53:52 +0100

    Implement save page

--- kindler/api/wikipedia.py
+++ kindler/api/wikipedia.py
@@ -1,14 +1,22 @@
-import requests
 import logging
-from flask import render_template, Blueprint, request, redirect, url_for
+import os
+import subprocess
+import tempfile
 
-from kindler.config import GUTENDEX_THIRD_PARTY_URL, GUTENDEX_SELF_HOST_URL
 import wikipediaapi
+from flask import redirect, url_for
+from flask import render_template, Blueprint, request, Response, send_file, abort
+from pathvalidate import sanitize_filename
 
 wikipedia_bp = Blueprint("wikipedia", __name__, url_prefix="/wikipedia")
 
-wiki = wikipediaapi.Wikipedia(user_agent='MyProjectName (merlin@example.com)', language='en',
-                                       extract_format=wikipediaapi.ExtractFormat.HTML)
+wiki = wikipediaapi.Wikipedia(
+    user_agent="Kindler (kasra@madadipouya.com)",
+    language="en",
+    extract_format=wikipediaapi.ExtractFormat.HTML,
+)
+
+allowed_formats = {"html", "epub", "mobi", "azw3"}
 
 
 @wikipedia_bp.route("/")
@@ -25,39 +33,101 @@ def search():
 @wikipedia_bp.route("/readability")
 def readability_page():
     query = request.args.get("q")
+    article = get_wikipedia_article(query)
+    return render_template(
+        "read_wikipedia.html",
+        query=query,
+        title=article["title"],
+        content=article["content"],
+        url=article["url"],
+    )
+
+
+@wikipedia_bp.route("/save_page")
+def save_page():
+    query = request.args.get("q")
+    save_format = request.args.get("format", "html")
+    if not query:
+        return "No page is provided", 400
+    if save_format not in allowed_formats:
+        abort(400, "Invalid format")
+
+    article = get_wikipedia_article(query)
+    html_content = render_template(
+        "read_save_formatted.html",
+        title=article["title"],
+        content=article["content"],
+        url=article["url"],
+    )
+    if "html" == save_format:
+        response = Response(html_content, mimetype="text/html")
+        response.headers["Content-Disposition"] = (
+            f"attachment; filename={sanitize_filename(article['title'] + '.html')}"
+        )
+        return response
+    else:
+        with tempfile.NamedTemporaryFile(
+            suffix=".html", delete=False, mode="w", encoding="utf-8"
+        ) as html_tmp:
+            html_tmp.write(html_content)
+            input_html_file = html_tmp.name
+        with tempfile.NamedTemporaryFile(
+            suffix=f".{save_format}", delete=False
+        ) as output_tmp:
+            output_file = output_tmp.name
+        try:
+            subprocess.run(
+                [
+                    "ebook-convert",
+                    input_html_file,
+                    output_file,
+                    "--title",
+                    article["title"],
+                    "--authors",
+                    "Wikipedia",
+                    "--chapter",
+                    "//h2",
+                    "--level1-toc",
+                    "//h1",
+                    "--chapter-mark",
+                    "pagebreak",
+                ],
+                check=True,
+            )
+            download_file_name = sanitize_filename(f"{article['title']}.{save_format}")
+            response = send_file(
+                output_file, as_attachment=True, download_name=download_file_name
+            )
+
+            @response.call_on_close
+            def cleanup():
+                for f in [input_html_file, output_file]:
+                    try:
+                        os.remove(f)
+                        logging.info(f"File {f} deleted")
+                    except OSError:
+                        pass
+
+            return response
+        except subprocess.CalledProcessError as e:
+            os.remove(input_html_file)
+            if os.path.exists(output_file):
+                os.remove(output_file)
+            abort(500, f"Conversion failed: {e}")
+
+
+def get_wikipedia_article(query):
+    article = {}
     result = wiki.page(query)
     links = result.links
     content = result.text
     for title in links.keys():
-        content = content.replace(title, f"<a href={url_for("wikipedia.readability_page", q=title)}>{title}</a>", 1)
-    return render_template("read_wikipedia.html", query=query, title=result.title, content=content, url = result.fullurl)
-
-
-def search_book_from_gutendex_api(query):
-    try:
-        response = requests.get(
-            GUTENDEX_SELF_HOST_URL, params={"search": query}, timeout=5
+        content = content.replace(
+            title,
+            f"<a href={url_for("wikipedia.readability_page", q=title)}>{title}</a>",
+            1,
         )
-        logging.info(f"Successfully called self-hosted Gutendex for: '{query}' keyword")
-        return response
-    except (requests.ConnectionError, requests.Timeout):
-        logging.info(
-            f"Failed to call self-hosted Gutendex for: '{query}' keyword. Trying third-party now"
-        )
-        return requests.get(
-            GUTENDEX_THIRD_PARTY_URL, params={"search": query}, timeout=5
-        )
-
-
-def retrieve_book_details_by_id_from_gutendex_api(book_id):
-    try:
-        response = requests.get(f"{GUTENDEX_SELF_HOST_URL}{book_id}", timeout=5)
-        logging.info(
-            f"Successfully called self-hosted Gutendex to retrieve book details of book_id: '{book_id}'"
-        )
-        return response
-    except (requests.ConnectionError, requests.Timeout):
-        logging.info(
-            f"Failed to call self-hosted Gutendex to retrieve book details of book_id: '{book_id}'. Trying third-party now"
-        )
-        return requests.get(f"{GUTENDEX_THIRD_PARTY_URL}{book_id}", timeout=5)
+    article["content"] = content
+    article["title"] = result.title
+    article["url"] = result.fullurl
+    return article
